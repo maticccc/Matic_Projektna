619b93e93db51f1a84370b15f279bd5d26ba9860
Matic Curk <matic.curk@gmail.com>
2023-10-09 16:27:07 +0200

    Izdelovanje slovarja

--- zajem.py
+++ zajem.py
@@ -19,7 +19,7 @@ def url_v_niz(url):
 def shrani_niz_v_datoteko(niz, datoteka):
     with open(datoteka, 'w', encoding='utf-8') as dat:
         dat.write(niz)
-
+    return None
 
 def preberi_in_shrani_url(url, datoteka):
     niz = url_v_niz(url)
@@ -41,8 +41,24 @@ def preberi_in_izlusci_vrstice(datoteka):
     besedilo = preberi_datoteko(datoteka)
     vrstice = izlusci_vrstice(besedilo)
     return vrstice
-preberi_in_izlusci_vrstice('primerjava_drzav.html')
+
+vrstice = preberi_in_izlusci_vrstice('primerjava_drzav.html')
 # funckija vrne vrstice oblike <tr>...</tr>, kjer so zajeti podatki iz spletne strani
+
+def izlusci_vse_podatke(vrstice_tabele):
+    slovar = {}
+    vzorec_drzava = r'">(.*?)</a>'
+    drzave = re.findall(vzorec_drzava, vrstice_tabele)
+    for drzava in drzave:
+        slovar['Drzava'] = drzava
+    vzorec_vseh_treh_stevil = r'<td>(.+?)</td>'
+    stevila = re.findall(vzorec_vseh_treh_stevil, vrstice_tabele)
+    slovar['Indeks stroskov'] = stevila[2]
+    slovar['Mesecni dohodek'] = stevila[3]
+    slovar['Indeks kupne moci'] = stevila[4]
+
+    return slovar
+
 
 
 
