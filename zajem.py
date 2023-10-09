c8685c2cecbaafa3d3c7f28750c1c446f437daec
Matic Curk <matic.curk@gmail.com>
2023-10-09 16:16:27 +0200

    Pisanje funkcij

--- zajem.py
+++ zajem.py
@@ -25,3 +25,24 @@ def preberi_in_shrani_url(url, datoteka):
     niz = url_v_niz(url)
     shrani_niz_v_datoteko(niz, datoteka)
 
+preberi_in_shrani_url(url_spletne_strani, shrani_sem)
+
+def preberi_datoteko(datoteka):
+    with open(datoteka, 'r', encoding='utf-8') as dat:
+        return dat.read()
+
+def izlusci_vrstice(vsebina_strani):
+    vzorec_vrstice = r'<tr>(.*?)</tr>'
+    return re.findall(vzorec_vrstice, vsebina_strani, re.DOTALL)
+
+
+
+def preberi_in_izlusci_vrstice(datoteka):
+    besedilo = preberi_datoteko(datoteka)
+    vrstice = izlusci_vrstice(besedilo)
+    return vrstice
+preberi_in_izlusci_vrstice('primerjava_drzav.html')
+# funckija vrne vrstice oblike <tr>...</tr>, kjer so zajeti podatki iz spletne strani
+
+
+
